03fe037f14bebe34ced0f14e63ea94022efd3660
Shantvanu Mutha <122596382+shantvanu@users.noreply.github.com>
2025-11-19 19:26:11 +0000

    call apply and bind method 2

--- callapplybind.js
+++ callapplybind.js
@@ -10,9 +10,24 @@ let name2 = {
     age:25
 }
 
-function printName(){
-    console.log(this.firstName, this.lastName, this.age);
+function printName(city,state){
+    console.log(this.firstName, this.lastName, this.age, city, state);
 }
 
-printName.call(name1);
-printName.call(name2);
+// call method is used to borrow a function 
+
+printName.call(name1, "Jodhpur", "Rajasthan");
+printName.call(name2,"Patna","Bihar"); 
+
+// now in apply we just pass these arguments in an array instead of writing them separately.. see below
+
+printName.apply(name1,["Jodhpur","Rajasthan"]);
+printName.apply(name2,["Patna","Bihar"]);
+
+//Bind method is somewhat similar to call method, instead of executing the function instantly it keeps the copy of that function with the object to call it later
+
+let printMyName = printName.bind(name1,"Jodhpur","Rajasthan");
+console.log(printMyName);
+printMyName();
+
+
