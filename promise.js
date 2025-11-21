ac0a8a0bb7a87e4dbef01ad2ac8ce8c69b6258df
Shantvanu Mutha <122596382+shantvanu@users.noreply.github.com>
2025-11-21 11:02:02 +0000

    creating promise

---
+++ promise.js
@@ -0,0 +1,42 @@
+const cart = ["shoes", "tshirt", "jeans", "lower", "shirt"];
+createOrder(cart)
+  .then(function (orderId) {
+    console.log(orderId);
+    return orderId;
+  })
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  })
+  .then(function (orderId) {
+    console.log("i will anyway execute as i am outside the catch block");
+  });
+
+function createOrder(cart) {
+  const pr = new Promise(function (resolve, reject) {
+    if (!validateCart(cart)) {
+      const err = new Error("cart is not valid");
+      reject(err);
+    }
+    const orderId = "12345";
+    if (orderId) {
+      setTimeout(function () {
+        resolve(orderId);
+      }, 5000);
+    }
+  });
+
+  return pr;
+}
+
+function validateCart(cart) {
+  return true;
+}
+
+function proceedToPayment(orderId) {
+  return new Promise(function (resolve, reject) {
+    resolve("Payment Successful");
+  });
+}
